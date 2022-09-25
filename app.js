372f2fe7f759a0ff94b2f9d680de74e079ca0b7e
Tamplier322 <slavashishcov1@mail.ru>
2022-09-25 20:14:47 +0300

    adding tests

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 let express = require('express')
-
-
+let fortune = require('./lib/fortune.js')
 let app = express()
+
+app.get('/tours/hood-river', function(req, res){
+    res.render('tours/hood-river');
+});
+app.get('/tours/request-group-rate', function(req, res){
+    res.render('tours/request-group-rate');
+});
+
+app.use(function (req, res, next){
+    res.locals.showTests = app.get('env') !== 'production' &&
+        req.query.test === '1'
+    next()
+})
+
 app.use(express.static(__dirname + '/public'))
 let handlebars = require('express-handlebars')
     .create({defaultLayout:'main'})
@@ -13,8 +26,11 @@ app.set('port', process.env.PORT || 3000)
 app.get('/', function (req, res) {
     res.render('home')
 })
-app.get('/about', function (req, res) {
-    res.render('about')
+app.get("/about", function (req, res) {
+    res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
+    } );
 })
 
 app.use(function(req, res, next){
---
+++ qa/tests-crosspage.js
@@ -0,0 +1,39 @@
+let Browser = require('zombie'),
+    assert = require('chai').assert;
+let browser;
+suite('Межстраничные тесты', function(){
+    setup(function(){
+        browser = new Browser();
+    });
+    test('запрос расценок для групп со страницы туров по реке Худ'
+        + 'должен заполнять поле реферера', function(done){
+        let referrer = 'http://localhost:3000/tours/hood-river';
+        browser.visit(referrer, function(){
+            browser.clickLink('.requestGroupRate', function(){
+                assert(browser.field('referrer').value
+                    === referrer);
+                done();
+            });
+        });
+    });
+    test('запрос расценок для групп со страницы туров '
+        + 'пансионата "Орегон Коуст" должен '
+        + 'заполнять поле реферера', function(done){
+        let referrer = 'http://localhost:3000/tours/oregon-coast';
+        browser.visit(referrer, function(){
+            browser.clickLink('.requestGroupRate', function(){
+                assert(browser.field('referrer').value
+                    === referrer);
+                done();
+            });
+        });
+    });
+    test('посещение страницы "Запрос цены для групп" напрямую '
+        + 'должен приводить к пустому полю реферера', function(done){
+        browser.visit('http://localhost:3000/tours/request-group-rate',
+            function(){
+                assert(browser.field('referrer').value=== '');
+                done();
+            });
+    });
+});

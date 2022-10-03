bbd5826a5ff717cd44fbb3beebee5e916a7ac1a2
Tamplier322 <slavashishcov1@mail.ru>
2022-10-03 17:50:37 +0300

    handlebars weather

--- app.js
+++ app.js
@@ -1,6 +1,52 @@
 let express = require('express')
 let fortune = require('./lib/fortune.js')
 let app = express()
+app.disable('x-powered-by');
+
+function getWeatherData(){
+    return{
+        locations: [
+            {
+                name: 'Портленд',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Сплошная облачность',
+                teml: '12.3 C',
+            },
+            {
+                name: 'Бенд',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Малооблачно',
+                temp: '55.0 F (12.8 C)',
+            },
+            {
+                name: 'Манзанита',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Небольшой дождь',
+                temp: '55.0 F (12.8 C)',
+            },
+        ],
+    };
+}
+
+app.use(function(req, res, next){
+    if(!res.locals.partials) res.locals.partials = {};
+    res.locals.partials.weatherContext = getWeatherData()
+    next()
+})
+
+
+
+let tours = [
+    { id: 0, name: 'Река Худ', price: 99.99 },
+    { id: 1, name: 'Орегон Коуст', price: 149.95 },
+];
+
+app.get('/api/tours', function(req, res){
+    res.json(tours);
+});
 
 app.get('/tours/hood-river', function(req, res){
     res.render('tours/hood-river');
@@ -21,11 +67,14 @@ let handlebars = require('express-handlebars')
 app.engine('handlebars', handlebars.engine)
 app.set('view engine', 'handlebars')
 
+//ставим порт 3000
 app.set('port', process.env.PORT || 3000)
 
+//выдает главную страницу
 app.get('/', function (req, res) {
     res.render('home')
 })
+//выдает страницу about
 app.get("/about", function (req, res) {
     res.render('about', {
         fortune: fortune.getFortune(),
@@ -33,17 +82,25 @@ app.get("/about", function (req, res) {
     } );
 })
 
+//headers http запроса
+// app.get('/headers', function(req,res){
+//     res.set('Content-Type','text/plain');
+//     var s = '';
+//     for(var name in req.headers)
+//         s += name + ': ' + req.headers[name] + '\n';
+//     res.send(s);
+// })
+//страница ошибки 404
 app.use(function(req, res, next){
     res.status(404)
     res.render('404')
 })
-
+//страница ошибки 500
 app.use(function( req, res, next, err){
     console.error(err.stack)
     res.status(500)
     res.render('500')
 })
-
 
 
 app.listen(app.get('port'), function () {
